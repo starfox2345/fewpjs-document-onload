cc46c3c74adc2228a310ddfa49386fbf9b3f7420
yatsun <yatsunlam25@gmail.com>
2021-08-29 20:58:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-    updateDOM()
+    updateText()
 });
 
-function updateDOM() {
+function updateText() {
     document.getElementById("text")
     .innerHTML = "This is really cool!";
 }
